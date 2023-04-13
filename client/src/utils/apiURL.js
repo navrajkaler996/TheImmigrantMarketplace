ae0ed7b3da91729e2bdc914e30e9853d2e8ba6ff
Navraj singh kaler <navrajkaler996@gmail.com>
2023-04-13 18:00:34 -0500

    base url updated

--- client/src/utils/apiURL.js
+++ client/src/utils/apiURL.js
@@ -1,8 +1,8 @@
 export const getBaseURL = () => {
   let API_URL;
   console.log(window.location.host);
-  if (window.location.host === "immigrantmarketplace") {
-    API_URL = "https://immigrantmarketplace-api";
+  if (window.location.host === "https://immigrantmarketplace.onrender") {
+    API_URL = "https://immigrantmarketplace-api.onrender";
   }
 
   if (window.location.host === "localhost:3000") {
