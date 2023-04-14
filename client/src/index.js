4e4d2d6dd43790b484a2e1db0b1a30d8a3a083c7
Navraj singh kaler <navrajkaler996@gmail.com>
2023-04-13 22:27:46 -0500

    changes-2

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -10,9 +10,9 @@ import "./index.css";
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
